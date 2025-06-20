9e799558584f7ffb4a2beef05445ff7c970f3bde
mtriet1337 <minhtriet1337@gmail.com>
2025-06-20 18:08:18 +0700

    Add files via upload

---
+++ device_oppo_cph1969/lineage_cph1969.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/oppo/cph1969/device.mk)
+
+PRODUCT_DEVICE := cph1969
+PRODUCT_NAME := lineage_cph1969
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := F11 Pro
+PRODUCT_MANUFACTURER := OPPO
